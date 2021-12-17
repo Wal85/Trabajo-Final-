1471f7eba45f0286d908936a8ff42387d099eaa4
Wal85 <stayalive349@gmail.com>
2021-12-17 19:22:59 -0300

    Trabajo Final

---
+++ script.js
@@ -0,0 +1,16 @@
+function mostrarFormulario (){
+
+    document.getElementById('formulario').style.display='block';
+      
+  }
+  
+  function mostrarRegistro (){
+     
+    alert('Se ha registrado correctamente');
+    
+  } 
+  
+  
+  
+
+  
